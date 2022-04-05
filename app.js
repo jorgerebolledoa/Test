44b9b9cdfb17fe721ae96ef41a49aadc39f52e99
jorgerebolledoa <aziel.az18@gmail.com>
2022-04-05 20:56:19 +0000

    Test

---
+++ app.js
@@ -0,0 +1,16 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+// declaramos una funcion con el mismo nombre "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convertimos el valor a dolares
+    let valueInDollar = valueInEuro * 1.2;
+    // retornamos el valor
+    return valueInDollar;
+}
+// tenemos que incluir la funcion en el exports para que sea exportada a otros archivos como test.js
+module.exports = { sum, fromEuroToDollar }
